0367b62485e39c85cc68778504e4905a83586d55
Jan Funke <funke@ini.ch>
2015-05-24 11:58:12 -0400

    solve: read features, and set edge weights from mean intensity differences

--- binaries/solve.cpp
+++ binaries/solve.cpp
@@ -12,6 +12,8 @@
 #include <io/Hdf5CragStore.h>
 #include <inference/MultiCut.h>
 
+#include <vigra/functorexpression.hxx>
+
 util::ProgramOption optionFeatureWeights(
 		util::_long_name        = "featureWeights",
 		util::_short_name       = "f",
@@ -31,54 +33,132 @@ int main(int argc, char** argv) {
 		util::ProgramOptions::init(argc, argv);
 		logger::LogManager::init();
 
-		//Hdf5CragStore cragStore(optionProjectFile.as<std::string>());
-
-		//Crag crag;
-		//cragStore.retrieveCrag(crag);
-
-		//NodeFeatures nodeFeatures(crag);
-		//EdgeFeatures edgeFeatures(crag);
-
-		//cragStore.retrieveNodeFeatures(crag, nodeFeatures);
-		//cragStore.retrieveEdgeFeatures(crag, edgeFeatures);
+		Hdf5CragStore cragStore(optionProjectFile.as<std::string>());
 
 		Crag crag;
+		cragStore.retrieveCrag(crag);
 
-		Crag::Node a = crag.addNode();
-		Crag::Node b = crag.addNode();
-		Crag::Node c = crag.addNode();
-		Crag::Node d = crag.addNode();
-		Crag::Node e = crag.addNode();
+		NodeFeatures nodeFeatures(crag);
+		EdgeFeatures edgeFeatures(crag);
 
-		Crag::Edge cd = crag.addAdjacencyEdge(c, d);
-		Crag::Edge de = crag.addAdjacencyEdge(d, e);
-		Crag::Edge be = crag.addAdjacencyEdge(b, e);
-		Crag::Edge ce = crag.addAdjacencyEdge(c, e);
+		cragStore.retrieveNodeFeatures(crag, nodeFeatures);
+		cragStore.retrieveEdgeFeatures(crag, edgeFeatures);
 
-		crag.addSubsetArc(c, b);
-		crag.addSubsetArc(d, b);
-		crag.addSubsetArc(b, a);
-		crag.addSubsetArc(e, a);
+		//Crag crag;
+
+		//Crag::Node a = crag.addNode();
+		//Crag::Node b = crag.addNode();
+		//Crag::Node c = crag.addNode();
+		//Crag::Node d = crag.addNode();
+		//Crag::Node e = crag.addNode();
+
+		//Crag::Edge cd = crag.addAdjacencyEdge(c, d);
+		//Crag::Edge de = crag.addAdjacencyEdge(d, e);
+		//Crag::Edge be = crag.addAdjacencyEdge(b, e);
+		//Crag::Edge ce = crag.addAdjacencyEdge(c, e);
+
+		//crag.addSubsetArc(c, b);
+		//crag.addSubsetArc(d, b);
+		//crag.addSubsetArc(b, a);
+		//crag.addSubsetArc(e, a);
 
 		Crag::NodeMap<double> nodeWeights(crag);
 		Crag::EdgeMap<double> edgeWeights(crag);
 
-		nodeWeights[a] = 0;
-		nodeWeights[b] = 0;
-		nodeWeights[c] = 0;
-		nodeWeights[d] = 0;
-		nodeWeights[e] = 0;
+		//nodeWeights[a] = 0;
+		//nodeWeights[b] = 0;
+		//nodeWeights[c] = 0;
+		//nodeWeights[d] = 0;
+		//nodeWeights[e] = 0;
 
-		edgeWeights[cd] = -1;
-		edgeWeights[de] =  1;
-		edgeWeights[be] = -0.5;
-		edgeWeights[ce] = -0.5;
+		//edgeWeights[cd] = -1;
+		//edgeWeights[de] =  1;
+		//edgeWeights[be] = -0.5;
+		//edgeWeights[ce] = -0.5;
+
+		for (Crag::NodeIt n(crag); n != lemon::INVALID; ++n)
+			nodeWeights[n] = 0;
+
+		float bias = -3000;
+		for (Crag::EdgeIt e(crag); e != lemon::INVALID; ++e) {
+
+			float intensityU = nodeFeatures[crag.u(e)][4];
+			float intensityV = nodeFeatures[crag.v(e)][4];
+
+			edgeWeights[e] = bias + pow(intensityU - intensityV, 2);
+		}
 
 		MultiCut multicut(crag);
 
 		multicut.setNodeWeights(nodeWeights);
 		multicut.setEdgeWeights(edgeWeights);
 		multicut.solve();
+
+		//////////////////
+		// IMAGE OUTPUT //
+		//////////////////
+
+		util::box<float, 3> cragBB = crag.getBoundingBox();
+		util::point<float, 3> resolution;
+		for (Crag::NodeIt n(crag); n != lemon::INVALID; ++n) {
+
+			resolution = crag.getVolumes()[n].getResolution();
+			break;
+		}
+
+		// create a vigra multi-array large enough to hold all volumes
+		vigra::MultiArray<3, int> components(
+				vigra::Shape3(
+					cragBB.width() /resolution.x(),
+					cragBB.height()/resolution.y(),
+					cragBB.depth() /resolution.z()),
+				std::numeric_limits<int>::max());
+
+		for (Crag::NodeIt n(crag); n != lemon::INVALID; ++n) {
+
+			if (crag.getVolumes()[n].getDiscreteBoundingBox().isZero())
+				continue;
+
+			const util::point<float, 3>&      volumeOffset     = crag.getVolumes()[n].getOffset();
+			const util::box<unsigned int, 3>& volumeDiscreteBB = crag.getVolumes()[n].getDiscreteBoundingBox();
+
+			util::point<unsigned int, 3> begin = (volumeOffset - cragBB.min())/resolution;
+			util::point<unsigned int, 3> end   = begin +
+					util::point<unsigned int, 3>(
+							volumeDiscreteBB.width(),
+							volumeDiscreteBB.height(),
+							volumeDiscreteBB.depth());
+
+			vigra::combineTwoMultiArrays(
+					crag.getVolumes()[n].data(),
+					components.subarray(
+							vigra::Shape3(
+									begin.x(),
+									begin.y(),
+									begin.z()),
+							vigra::Shape3(
+									end.x(),
+									end.y(),
+									end.z())),
+					components.subarray(
+							vigra::Shape3(
+									begin.x(),
+									begin.y(),
+									begin.z()),
+							vigra::Shape3(
+									end.x(),
+									end.y(),
+									end.z())),
+					vigra::functor::ifThenElse(
+							vigra::functor::Arg1() == vigra::functor::Param(1),
+							vigra::functor::Param(multicut.getComponents()[n]),
+							vigra::functor::Arg2()
+					));
+		}
+
+		vigra::exportImage(
+				components.bind<2>(0),
+				vigra::ImageExportInfo("solution.tif"));
 
 	} catch (boost::exception& e) {
 
