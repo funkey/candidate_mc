679abdad73521a318e7105be85f65ff812490f3e
Jan Funke <funke@ini.ch>
2015-05-30 11:29:37 -0700

    extract_features: read intensities from project file

--- binaries/extract_features.cpp
+++ binaries/extract_features.cpp
@@ -10,6 +10,7 @@
 #include <util/ProgramOptions.h>
 #include <util/exceptions.h>
 #include <io/Hdf5CragStore.h>
+#include <io/Hdf5VolumeStore.h>
 #include <features/FeatureExtractor.h>
 
 util::ProgramOption optionProjectFile(
@@ -17,42 +18,6 @@ util::ProgramOption optionProjectFile(
 		util::_short_name       = "p",
 		util::_description_text = "The treemc project file.",
 		util::_default_value    = "project.hdf");
-
-util::ProgramOption optionRawImage(
-		util::_long_name        = "rawImage",
-		util::_short_name       = "r",
-		util::_description_text = "The raw image.",
-		util::_default_value    = "raw.tif");
-
-util::ProgramOption optionResX(
-		util::_long_name        = "resX",
-		util::_description_text = "The x resolution of one pixel in the raw image.",
-		util::_default_value    = 1);
-
-util::ProgramOption optionResY(
-		util::_long_name        = "resY",
-		util::_description_text = "The y resolution of one pixel in the raw image.",
-		util::_default_value    = 1);
-
-util::ProgramOption optionResZ(
-		util::_long_name        = "resZ",
-		util::_description_text = "The z resolution of one pixel in the raw image.",
-		util::_default_value    = 1);
-
-util::ProgramOption optionOffsetX(
-		util::_long_name        = "offsetX",
-		util::_description_text = "The x offset of the raw image.",
-		util::_default_value    = 0);
-
-util::ProgramOption optionOffsetY(
-		util::_long_name        = "offsetY",
-		util::_description_text = "The y offset of the raw image.",
-		util::_default_value    = 0);
-
-util::ProgramOption optionOffsetZ(
-		util::_long_name        = "offsetZ",
-		util::_description_text = "The z offset of the raw image.",
-		util::_default_value    = 0);
 
 int main(int argc, char** argv) {
 
@@ -69,22 +34,9 @@ int main(int argc, char** argv) {
 		NodeFeatures nodeFeatures(crag);
 		EdgeFeatures edgeFeatures(crag);
 
-		util::point<float, 3> resolution(
-				optionResX,
-				optionResY,
-				optionResZ);
-		util::point<float, 3> offset(
-				optionOffsetX,
-				optionOffsetY,
-				optionOffsetZ);
-
-		// get information about the image to read
-		std::string filename = optionRawImage;
-		vigra::ImageImportInfo info(filename.c_str());
-		ExplicitVolume<float> raw(info.width(), info.height(), 1);
-		importImage(info, raw.data().bind<2>(0));
-		raw.setResolution(resolution);
-		raw.setOffset(offset);
+		Hdf5VolumeStore volumeStore(optionProjectFile.as<std::string>());
+		ExplicitVolume<float> raw;
+		volumeStore.retrieveIntensities(raw);
 
 		FeatureExtractor featureExtractor(crag, raw);
 		featureExtractor.extract(
