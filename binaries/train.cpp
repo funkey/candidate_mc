46341a199d75a6e261b4ac2a86efb5aeee889a8a
Jan Funke <funke@ini.ch>
2015-07-10 11:13:54 -0400

    train: added option to read best-effort from hdf file

--- binaries/train.cpp
+++ binaries/train.cpp
@@ -31,13 +31,16 @@ util::ProgramOption optionFeatureWeights(
 		util::_description_text = "A file to store the learnt feature weights.",
 		util::_default_value    = "feature_weights.txt");
 
+util::ProgramOption optionBestEffortFromProjectFile(
+		util::_long_name        = "bestEffortFromProjectFile",
+		util::_description_text = "Read the best effort solution from the project file.");
+
 util::ProgramOption optionLoss(
 		util::_long_name        = "loss",
 		util::_description_text = "The loss to use for training: hamming (Hamming distance "
 		                          "to best effort, default) or overlap (RAND index approximation "
 		                          "to ground-truth).",
 		util::_default_value    = "hamming");
-
 
 util::ProgramOption optionNormalizeLoss(
 		util::_long_name        = "normalizeLoss",
@@ -73,13 +76,54 @@ int main(int argc, char** argv) {
 		parameters.epsStrategy = BundleOptimizer::EpsFromGap;
 		BundleOptimizer optimizer(parameters);
 
-		LOG_USER(logger::out) << "reading ground-truth" << std::endl;
-		ExplicitVolume<int> groundTruth;
-		volumeStore.retrieveGroundTruth(groundTruth);
+		BestEffort*  bestEffort  = 0;
+		OverlapLoss* overlapLoss = 0;
 
-		LOG_USER(logger::out) << "finding best-effort solution" << std::endl;
-		OverlapLoss overlapLoss(crag, groundTruth);
-		BestEffort bestEffort(crag, overlapLoss);
+		if (optionBestEffortFromProjectFile) {
+
+			LOG_USER(logger::out) << "reading best-effort" << std::endl;
+
+			bestEffort = new BestEffort(crag);
+
+			vigra::HDF5File project(
+					optionProjectFile.as<std::string>(),
+					vigra::HDF5File::OpenMode::ReadWrite);
+			project.cd("best_effort");
+			vigra::ArrayVector<int> beNodes;
+			vigra::MultiArray<2, int> beEdges;
+			project.readAndResize("nodes", beNodes);
+			project.readAndResize("edges", beEdges);
+
+			std::set<int> nodes;
+			for (int n : beNodes)
+				nodes.insert(n);
+
+			std::set<std::pair<int, int>> edges;
+			for (int i = 0; i < beEdges.shape(1); i++)
+				edges.insert(
+						std::make_pair(
+							std::min(beEdges(i, 0), beEdges(i, 1)),
+							std::max(beEdges(i, 0), beEdges(i, 1))));
+
+			for (Crag::NodeIt n(crag); n != lemon::INVALID; ++n)
+				bestEffort->node[n] = nodes.count(crag.id(n));
+
+			for (Crag::EdgeIt e(crag); e != lemon::INVALID; ++e)
+				bestEffort->edge[e] = edges.count(
+						std::make_pair(
+								std::min(crag.id(crag.u(e)), crag.id(crag.v(e))),
+								std::max(crag.id(crag.u(e)), crag.id(crag.v(e)))));
+
+		} else {
+
+			LOG_USER(logger::out) << "reading ground-truth" << std::endl;
+			ExplicitVolume<int> groundTruth;
+			volumeStore.retrieveGroundTruth(groundTruth);
+
+			LOG_USER(logger::out) << "finding best-effort solution" << std::endl;
+			overlapLoss = new OverlapLoss(crag, groundTruth);
+			bestEffort = new BestEffort(crag, *overlapLoss);
+		}
 
 		Loss* loss = 0;
 		bool  destructLoss = false;
@@ -88,14 +132,24 @@ int main(int argc, char** argv) {
 
 			LOG_USER(logger::out) << "using Hamming loss" << std::endl;
 
-			loss = new HammingLoss(crag, bestEffort);
+			loss = new HammingLoss(crag, *bestEffort);
 			destructLoss = true;
 
 		} else if (optionLoss.as<std::string>() == "overlap") {
 
 			LOG_USER(logger::out) << "using overlap loss" << std::endl;
 
-			loss = &overlapLoss;
+			if (!overlapLoss) {
+
+				LOG_USER(logger::out) << "reading ground-truth" << std::endl;
+				ExplicitVolume<int> groundTruth;
+				volumeStore.retrieveGroundTruth(groundTruth);
+
+				LOG_USER(logger::out) << "finding best-effort solution" << std::endl;
+				overlapLoss = new OverlapLoss(crag, groundTruth);
+			}
+
+			loss = overlapLoss;
 
 		} else {
 
@@ -117,7 +171,7 @@ int main(int argc, char** argv) {
 				nodeFeatures,
 				edgeFeatures,
 				*loss,
-				bestEffort);
+				*bestEffort);
 
 		std::vector<double> weights(nodeFeatures.dims() + edgeFeatures.dims(), 0);
 		optimizer.optimize(oracle, weights);
@@ -126,6 +180,12 @@ int main(int argc, char** argv) {
 
 		if (destructLoss && loss != 0)
 			delete loss;
+
+		if (overlapLoss)
+			delete overlapLoss;
+
+		if (bestEffort)
+			delete bestEffort;
 
 	} catch (boost::exception& e) {
 
