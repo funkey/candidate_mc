7cdd059fce51c6f1d233b5e5fbc44312be63ccca
Jan Funke <funke@ini.ch>
2015-07-31 11:13:57 -0400

    added more solver tests for scip (fails due to bug in gurobi, workaround needed)

--- binaries/tests/solver/backends.cpp
+++ binaries/tests/solver/backends.cpp
@@ -2,6 +2,10 @@
 #include <tests.h>
 #include <solver/DefaultFactory.h>
 
+#ifdef HAVE_GUROBI
+#include <gurobi_c++.h>
+#endif
+
 namespace backends_test {
 
 int numVars = 10;
@@ -21,7 +25,7 @@ void testSolver(LinearSolverBackend* solver) {
 	solver->setConstraints(constraints);
 	solver->solve(x, _);
 
-	BOOST_CHECK_EQUAL(x.getValue(), -4);
+	BOOST_CHECK_EQUAL(x.getValue(), -3999);
 	BOOST_CHECK_EQUAL(x[numVars - 1], 1);
 	for (int i = 0; i < numVars - 1; i++)
 		BOOST_CHECK_EQUAL(x[i], 0);
@@ -31,7 +35,7 @@ void testSolver(LinearSolverBackend* solver) {
 	solver->setConstraints(constraints);
 	solver->solve(x, _);
 
-	BOOST_CHECK_EQUAL(x.getValue(), 5);
+	BOOST_CHECK_EQUAL(x.getValue(), 5001);
 	BOOST_CHECK_EQUAL(x[0], 1);
 	for (int i = 1; i < numVars; i++)
 		BOOST_CHECK_EQUAL(x[i], 0);
@@ -42,7 +46,8 @@ void testSolver(LinearSolverBackend* solver) {
 void backends() {
 
 	for (int i = 0; i < numVars; i++)
-		objective.setCoefficient(i, 5 - i%10);
+		objective.setCoefficient(i, 1000*(5 - i%10));
+	objective.setConstant(1);
 
 	LinearConstraint onlyOneConstraint;
 	for (int i = 0; i < numVars; i++)
@@ -61,12 +66,36 @@ void backends() {
 		testSolver(solver);
 		delete solver;
 	}
+
+	{
+		GRBEnv env;
+		GRBModel model(env);
+		GRBVar* vars = model.addVars(3, GRB_BINARY);
+		model.update();
+		GRBQuadExpr objective = 1 + -1000*vars[0] + 0*vars[1] + 2000*vars[2];
+		std::cout << "gurobi objective: " << objective << std::endl;
+		model.setObjective(objective, GRB_MINIMIZE);
+		model.update();
+		std::cout << "gurobi objective after setting to minimize: " << model.getObjective() << std::endl;
+		model.setObjective(objective, GRB_MAXIMIZE);
+		model.update();
+		std::cout << "gurobi objective after setting to maximize: " << model.getObjective() << std::endl;
+	}
 #endif
 
 #ifdef HAVE_CPLEX
 	{
 		std::cout << "testing cplex solver" << std::endl;
 		LinearSolverBackend* solver = factory.createLinearSolverBackend(Cplex);
+		testSolver(solver);
+		delete solver;
+	}
+#endif
+
+#ifdef HAVE_SCIP
+	{
+		std::cout << "testing scip solver" << std::endl;
+		LinearSolverBackend* solver = factory.createLinearSolverBackend(Scip);
 		testSolver(solver);
 		delete solver;
 	}
