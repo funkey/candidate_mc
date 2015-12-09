c52c98e8f7d47749339b0435334618c21574dacf
Jan Funke <jfunke@visen2.users.iri.prv>
2015-12-09 15:44:31 +0100

    bugfix: CragSolution::label()
    1. did not return label != 0 for connected components (the first
    component was labeled 0)
    2. did not return 0 for non-selected candidates (they were forming
    connected components themselves, and thus got a label)

--- inference/CragSolution.cpp
+++ inference/CragSolution.cpp
@@ -6,13 +6,32 @@ CragSolution::findConnectedComponents() const {
 
 	typedef lemon::ListGraph Cut;
 	Cut labelGraph;
-	for (unsigned int i = 0; i < _crag.nodes().size(); i++)
-		labelGraph.addNode();
 
+	// create a cut graph, i.e., a graph of only selected nodes and edges
+
+	// add only selected nodes, remember mapping from original nodes to cut 
+	// graph nodes
+	Crag::NodeMap<Cut::Node> cutGraphNodes(_crag);
+	for (unsigned int i = 0; i < _crag.nodes().size(); i++)
+	for (Crag::CragNode n : _crag.nodes())
+		if (_selectedNodes[n])
+			cutGraphNodes[n] = labelGraph.addNode();
+
+	// add selected edges
 	for (Crag::CragEdge e : _crag.edges())
 		if (_selectedEdges[e] && _crag.type(e) != Crag::NoAssignmentEdge)
-			labelGraph.addEdge(e.u(), e.v());
+			labelGraph.addEdge(
+					cutGraphNodes[e.u()],
+					cutGraphNodes[e.v()]);
 
 	// find connected components in cut graph
-	lemon::connectedComponents(labelGraph, _labels);
+	Cut::NodeMap<unsigned int> ccs(labelGraph);
+	lemon::connectedComponents(labelGraph, ccs);
+
+	// label original nodes
+	for (Crag::CragNode n : _crag.nodes())
+		if (_selectedNodes[n])
+			_labels[n] = ccs[cutGraphNodes[n]] + 1;
+		else
+			_labels[n] = 0;
 }
