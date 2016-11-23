94e27f08fb3d5eef538e6ddc08bc40e970de7141
Vanessa Leite <vrcleite@gmail.com>
2016-11-23 12:51:23 +0100

    Adding name of the propertie in which the features are been extracted

--- features/ContactFeatureProvider.h
+++ features/ContactFeatureProvider.h
@@ -11,10 +11,12 @@ public:
 	ContactFeatureProvider(
 			const Crag& crag,
 			const CragVolumes& volumes,
-			const ExplicitVolume<float>& values) :
+			const ExplicitVolume<float>& values,
+			const std::string valuesName = "values") :
 		_crag(crag),
 		_volumes (volumes),
-		_values(values){
+		_values(values),
+		_valuesName(valuesName){
 	}
 
 	template <typename ContainerT>
@@ -35,21 +37,21 @@ public:
 
 		for (int i = 0; i < 3/*number of thresholds*/; i++)
 		{
-			names[Crag::AdjacencyEdge].push_back("contact_score_edge_u_" + boost::lexical_cast<std::string>(i));
-			names[Crag::AdjacencyEdge].push_back("normalized_contact_score_edge_u_" + boost::lexical_cast<std::string>(i));
-			names[Crag::AdjacencyEdge].push_back("contact_score_edge_v_" + boost::lexical_cast<std::string>(i));
-			names[Crag::AdjacencyEdge].push_back("normalized_contact_score_edge_v_" + boost::lexical_cast<std::string>(i));
+			names[Crag::AdjacencyEdge].push_back(_valuesName + "_contact_score_edge_u_" + boost::lexical_cast<std::string>(i));
+			names[Crag::AdjacencyEdge].push_back(_valuesName + "_normalized_contact_score_edge_u_" + boost::lexical_cast<std::string>(i));
+			names[Crag::AdjacencyEdge].push_back(_valuesName + "_contact_score_edge_v_" + boost::lexical_cast<std::string>(i));
+			names[Crag::AdjacencyEdge].push_back(_valuesName + "_normalized_contact_score_edge_v_" + boost::lexical_cast<std::string>(i));
 
-			names[Crag::AdjacencyEdge].push_back("log_contact_score_edge_u_" + boost::lexical_cast<std::string>(i));
-			names[Crag::AdjacencyEdge].push_back("log_normalized_contact_score_edge_u_" + boost::lexical_cast<std::string>(i));
-			names[Crag::AdjacencyEdge].push_back("log_contact_score_edge_v_" + boost::lexical_cast<std::string>(i));
-			names[Crag::AdjacencyEdge].push_back("log_normalized_contact_score_edge_v_" + boost::lexical_cast<std::string>(i));
+			names[Crag::AdjacencyEdge].push_back(_valuesName + "_log_contact_score_edge_u_" + boost::lexical_cast<std::string>(i));
+			names[Crag::AdjacencyEdge].push_back(_valuesName + "_log_normalized_contact_score_edge_u_" + boost::lexical_cast<std::string>(i));
+			names[Crag::AdjacencyEdge].push_back(_valuesName + "_log_contact_score_edge_v_" + boost::lexical_cast<std::string>(i));
+			names[Crag::AdjacencyEdge].push_back(_valuesName + "_log_normalized_contact_score_edge_v_" + boost::lexical_cast<std::string>(i));
 		}
 
-		names[Crag::AdjacencyEdge].push_back("log_volume_ratio_u");
-		names[Crag::AdjacencyEdge].push_back("log_volume_ratio_v");
-		names[Crag::AdjacencyEdge].push_back("volume_ratio_u");
-		names[Crag::AdjacencyEdge].push_back("volume_ratio_v");
+		names[Crag::AdjacencyEdge].push_back(_valuesName + "_log_volume_ratio_u");
+		names[Crag::AdjacencyEdge].push_back(_valuesName + "_log_volume_ratio_v");
+		names[Crag::AdjacencyEdge].push_back(_valuesName + "_volume_ratio_u");
+		names[Crag::AdjacencyEdge].push_back(_valuesName + "_volume_ratio_v");
 
 		return names;
 	}
@@ -60,7 +62,8 @@ private:
 	const CragVolumes& _volumes;
 
 	const ExplicitVolume<float>& _values;
+	std::string _valuesName;
 };
 
-#endif // CANDIDATE_MC_FEATURES_TOPOLOGICAL_FEATURE_PROVIDER_H__
+#endif // CANDIDATE_MC_FEATURES_CONTACT_FEATURE_PROVIDER_H__
 
